c0d41bb39b0115bee1c7dfd687b9bb32d0849b16
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-05-17 12:50:45 -0500

    added ec_from_pub.c

---
+++ crypto/ec_from_pub.c
@@ -0,0 +1,38 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_from_pub - create an EC_KEY from a public key
+ * @pub: the octal public key
+ *
+ * Return: pointer to EC_KEY or NULL
+ */
+
+EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
+{
+	if (!pub)
+		return (NULL);
+
+	EC_POINT *point;
+	EC_KEY *key;
+	const EC_GROUP *group;
+
+	key = EC_KEY_new_by_curve_name(EC_CURVE);
+	group = EC_KEY_get0_group(key);
+	point = EC_POINT_new(group);
+
+	if (!EC_POINT_oct2point(group, point, pub, EC_PUB_LEN, NULL))
+	{
+		EC_POINT_free(point);
+		EC_KEY_free(key);
+		return (NULL);
+	}
+
+	if (!EC_KEY_set_public_key(key, point))
+	{
+		EC_POINT_free(point);
+		EC_KEY_free(key);
+		return (NULL);
+	}
+	EC_POINT_free(point);
+	return (key);
+}
