83a3a87a6912fffdcd414a9c79461400131ee784
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-05-25 11:46:35 -0500

    added check for sig->len

--- crypto/ec_verify.c
+++ crypto/ec_verify.c
@@ -16,7 +16,7 @@ int ec_verify(EC_KEY const *key,
 	      size_t msglen,
 	      sig_t const *sig)
 {
-	if (!key || !msg || !sig)
+	if (!key || !msg || !sig || sig->len < 1)
 		return (0);
 
 	if (!ECDSA_verify(0,
