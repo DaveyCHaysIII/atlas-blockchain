1e7ec747613ad807cc6f6e0daa6ac013958c93f5
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-06-05 18:10:58 -0500

    added block_create

---
+++ blockchain/v0.1/block_create.c
@@ -0,0 +1,38 @@
+#include "blockchain.h"
+
+/**
+ * block_create - creates a block
+ * @prev: a pointer to the previous block
+ * @data: the data to init in the new block
+ * @data_len: the length of the data we're using
+ *
+ * Return: pointer to the new block
+ */
+
+block_t *block_create(block_t const *prev,
+		      int8_t const *data,
+		      uint32_t data_len)
+{
+	if (!prev || !data || !data_len)
+		return (NULL);
+
+	block_t *new_block;
+
+	new_block = calloc(1, sizeof(block_t));
+	if (!new_block)
+		return (NULL);
+
+	new_block->info.index = (prev->info.index) + 1;
+	new_block->info.difficulty = 0;
+	new_block->info.timestamp = time(NULL);
+	new_block->info.nonce = 0;
+	memcpy(new_block->info.prev_hash,
+	       prev->hash,
+	       SHA256_DIGEST_LENGTH);
+	memcpy(new_block->data.buffer,
+	       data,
+	       data_len > BLOCKCHAIN_DATA_MAX ? BLOCKCHAIN_DATA_MAX : data_len);
+	new_block->data.len = data_len;
+
+	return (new_block);
+}
--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -29,20 +29,20 @@ blockchain_t *blockchain_create(void)
 		free(genesis);
 		return (NULL);
 	}
-
 	genesis->info.index = 0;
 	genesis->info.difficulty = 0;
 	genesis->info.timestamp = GEN_TIMESTAMP;
 	genesis->info.nonce = 0;
 	memset(genesis->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
-
 	memcpy(genesis->hash, HOLBERTON_HASH, SHA256_DIGEST_LENGTH);
-
 	genesis->data.len = 16;
 	memcpy(genesis->data.buffer, DATA, genesis->data.len);
-
 	if (llist_add_node(new_chain->chain, genesis, ADD_NODE_REAR) == -1)
-		return (llist_destroy(new_chain->chain, 0, NULL), free(new_chain), NULL);
-
+	{
+		llist_destroy(new_chain->chain, 0, NULL);
+		free(new_chain);
+		free(genesis);
+		return (NULL);
+	}
 	return (new_chain);
 }
