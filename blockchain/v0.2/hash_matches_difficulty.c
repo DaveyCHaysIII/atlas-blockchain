c2e74b7cee0ba06f96f1fa13dedeafc42106a8ee
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-06-19 10:29:33 -0500

    added hash_matches_difficulty

--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -3,7 +3,7 @@
 /**
  * hash_matches_difficulty - checks the difficulty leading bits of hash
  * @hash: the hash in question
- * @difficutly: number of leading bits to check
+ * @difficulty: number of leading bits to check
  *
  * Return: 1 on success, 0 on failure
  */
@@ -11,5 +11,27 @@
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 			    uint32_t difficulty)
 {
+	uint32_t bytes, remain, i;
+	uint8_t mask;
 
+	if (!hash || !difficulty)
+		return (0);
+
+	bytes = difficulty / 8;
+	remain = difficulty % 8;
+	mask = 0xFF << (8 - remain);
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (hash[i] != 0)
+			return (0);
+	}
+
+	if (remain != 0)
+	{
+		if ((hash[i] & mask) != 0)
+			return (0);
+	}
+
+	return (1);
 }
