91e96035cf176b5191bbfdc4cd2cc279ffdf8333
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-07-05 13:18:59 -0500

    made minor change to block_destroy

--- blockchain/v0.3/block_destroy.c
+++ blockchain/v0.3/block_destroy.c
@@ -12,9 +12,13 @@ void block_destroy(block_t *block)
 	if (block)
 	{
 		if (llist_size(block->transactions) > 0)
-			llist_destroy(block->transactions, 1, NULL);
+			llist_destroy(block->transactions,
+				      1,
+				      (node_dtor_t)&transaction_destroy);
 		else
-			llist_destroy(block->transactions, 0, NULL);
+			llist_destroy(block->transactions,
+				      0,
+				      (node_dtor_t)&transaction_destroy);
 		free(block);
 	}
 }
