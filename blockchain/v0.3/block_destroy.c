2d54102731834e2d01395e1766ed93dd08e087ab
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2025-07-07 20:44:16 -0500

    blockchain delete test

--- blockchain/v0.3/block_destroy.c
+++ blockchain/v0.3/block_destroy.c
@@ -11,9 +11,10 @@ void block_destroy(block_t *block)
 {
 	if (!block)
 		return;
-	llist_destroy(block->transactions,
-		      1,
-		      (node_dtor_t)&transaction_destroy);
+	if (llist_size(block->transactions) > 0)
+		llist_destroy(block->transactions, 1, (node_dtor_t)&transaction_destroy);
+	else
+		llist_destroy(block->transactions, 0, (node_dtor_t)&transaction_destroy);
 	free(block);
 }
 
